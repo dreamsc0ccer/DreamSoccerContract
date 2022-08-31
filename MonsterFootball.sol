f4cebe03ee50a080409bc0068d2e400ef52b1ee4
dreamsc0ccer <112548430+dreamsc0ccer@users.noreply.github.com>
2022-08-31 16:28:04 +0700

    Create MonsterFootball.sol

---
+++ MonsterFootball.sol
@@ -0,0 +1,126 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+interface IFootballer {
+
+    function caculatorReward(uint256 nftID, uint256 difficult, address user) external view returns (uint256 reward);
+
+    function getNFTInformation(uint256 nftID) external view returns (uint256 Attribute, uint256 Energy, uint256 lastestUpdate);
+
+    function Training(uint256 nftID, address user) external;
+
+    function setNewTokenRewardAddress(address tokenReward) external;
+
+    function mint(uint256 attribute, address user) external;
+}
+
+
+contract MonsterFootball is ERC721, Ownable {
+
+    using SafeMath for uint256;
+
+    using Counters for Counters.Counter;
+
+    struct Monster {
+        uint256 difficult;
+        uint256 startTime;
+        uint256 endTime;
+    }
+
+    IFootballer _footballer;
+
+    Monster[] public Monsters;
+
+    Counters.Counter private currentTokenId;
+    
+    string public baseTokenURI;
+
+    address _operator;
+
+    constructor() ERC721("Monster", "NFT") {
+
+        baseTokenURI = "";
+
+    }
+
+    modifier onlyOperator() {
+
+        require(owner() == _msgSender() || _operator == _msgSender(), "Caller is not the Operator");	
+
+        _;	
+    }
+
+
+    function setFootballerAddress(address footballer) external onlyOperator {
+
+        _footballer = IFootballer(footballer);
+        
+    }
+
+    function footballerAddress() external view returns (address footballer) {
+        footballer = address(_footballer);
+    }
+
+    function operatorAddress() external view returns (address operator) {
+        operator = _operator;
+    }
+
+    function setOperator(address operator) external onlyOperator {
+        _operator = operator;
+    }
+
+    function callMonster() external onlyOperator {
+
+        uint256 newItemId = Monsters.length;
+        uint256 difficult = uint(keccak256(abi.encodePacked(block.number))).mod(3);
+        uint256 startTime = block.timestamp;
+        uint256 endTime = startTime + 5 minutes;
+        Monsters.push(Monster(difficult, startTime, endTime));
+        _safeMint(0x000000000000000000000000000000000000dEaD, newItemId);
+
+    }
+    
+    function getMonsterInformation(uint256 nftID) external view returns (uint256 difficult, uint256 startTime, uint256 endTime) {
+
+          difficult = Monsters[nftID].difficult;
+
+          startTime = Monsters[nftID].startTime;
+          
+          endTime = Monsters[nftID].endTime;
+          
+    }
+    function caculatorReward(uint256 nftID, uint256 monsterID) external view returns (uint256 reward) {
+
+        (uint256 Attribute, , ) = _footballer.getNFTInformation(nftID);
+
+        uint256 difficult = Monsters[monsterID].difficult;
+
+        difficult > 3 ? difficult = 2 : difficult = difficult;
+
+        uint256 bounus = ( 1 + difficult) * uint(keccak256(abi.encodePacked(block.number))).mod(3) * 10 ** 8;
+
+        if (Attribute == 0) {
+
+            reward = 100 * 2 * bounus;
+
+        } else if (Attribute == 1) {
+
+            reward = 200 * 2 * bounus;
+
+        } else {
+
+            reward = 500 * 2 * bounus;
+        }
+        
+
+        
+    }
+
+}
