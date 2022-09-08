5320bc305363dda1c6afdd3f59d61598cb5421c9
dreamsc0ccer <112548430+dreamsc0ccer@users.noreply.github.com>
2022-09-08 14:41:14 +0700

    Update Operator.sol

--- Operator.sol
+++ Operator.sol
@@ -47,8 +47,327 @@ interface IPices {
 }
 
 interface IShoes {
+
     function mint(address user, uint256 attr) external;
+
     function Pices() external view returns (address);
+
+    function getShoesInformation(uint256 shoesID) external view returns (uint256);
+
+    function ownerOf(uint256 tokenId) external view returns (address owner);
+
+    function transferFrom(address from, address to, uint256 tokenId) external;
+
+
+}
+
+contract StakingDSoccer is Context, Ownable {
+    using SafeMath for uint256;
+    
+    struct UserInfo {
+        uint256 _amountDeposite;
+        uint256 _timeDeposite;
+    }
+
+    mapping(address => UserInfo) private _userInfoMap;
+
+    address private _tokenReward;
+    uint256 private _totalToBeMintAmount;
+    uint256 private _APR = 365 * 24 * 60 * 20 * 100;
+
+    event AddTotalToBeMintAmount(address indexed user, uint256 pendingTotalToBeMintAmount, uint256 totalToBeMintAmount);
+
+    function addTotalToBeMintAmount(uint256 pendingTotalToBeMintAmount) external onlyOwner {
+        require(pendingTotalToBeMintAmount != 0);
+        ERC20(_tokenReward).transferFrom (msg.sender, address(this), pendingTotalToBeMintAmount);
+        _totalToBeMintAmount = _totalToBeMintAmount.add(pendingTotalToBeMintAmount);
+        emit AddTotalToBeMintAmount(msg.sender, pendingTotalToBeMintAmount, _totalToBeMintAmount);
+    }
+
+    function setAPR(uint256 APR) external onlyOwner {
+        _APR = APR;
+    }
+
+    function setTokenReward(address tokenReward) external onlyOwner {
+        _tokenReward = tokenReward;
+    }
+
+    function getReward(address user) public view returns (uint256) {
+
+        uint256 depositedTime = block.timestamp - _userInfoMap[msg.sender]._timeDeposite;
+
+        return _userInfoMap[user]._amountDeposite.mul(depositedTime).div(365 days).mul(_APR).div(100);
+
+    }
+
+    function getTotalToBeMintAmount() public view returns (uint256) {
+
+        return _totalToBeMintAmount;
+
+    }
+
+    function getAPR() public view returns (uint256) {
+
+        return _APR;
+
+    }
+
+    function getTotalReward(uint256 startBlock, uint256 endBlock, uint256 amountStake) public view returns (uint256 totalReward) {
+
+        uint256 timeStake = endBlock - startBlock;
+
+        return amountStake.mul(timeStake).div(365 days).mul(_APR).div(100);
+
+    }
+
+    function getUserInfo(address user) public view returns (uint256 amountDeposite, uint256 timeDeposite) {
+
+        return (_userInfoMap[user]._amountDeposite,_userInfoMap[user]._timeDeposite);
+    }
+
+    function stake(uint256 amount) external {
+        require(amount > 0, "Can not stake 0 token");
+
+        require(ERC20(_tokenReward).balanceOf(msg.sender) >= amount, "You do not have enough tokens to stake");
+
+
+        if (_userInfoMap[msg.sender]._amountDeposite != 0) {
+
+            if (getReward(msg.sender) != 0) {
+
+                if (_totalToBeMintAmount < getReward(msg.sender)){
+
+                    ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
+                    
+                } else {
+
+                    ERC20(_tokenReward).transfer(msg.sender, getReward(msg.sender));
+
+                    _totalToBeMintAmount = _totalToBeMintAmount - getReward(msg.sender);
+
+                }
+            }
+
+        }
+
+        ERC20(_tokenReward).transferFrom(msg.sender, address(this), amount);
+
+        _userInfoMap[msg.sender]._amountDeposite = _userInfoMap[msg.sender]._amountDeposite.add(amount);
+
+        _userInfoMap[msg.sender]._timeDeposite = block.timestamp;
+
+
+    }
+
+    function unstake(uint256 amount) external {
+
+        require(amount > 0, "Can not unstake 0 token");
+
+        require(_userInfoMap[msg.sender]._amountDeposite >= amount, "You do not have enough tokens to unstake");
+
+        if (_userInfoMap[msg.sender]._amountDeposite != 0) {
+
+            if (getReward(msg.sender) != 0) {
+                if (_totalToBeMintAmount < getReward(msg.sender)){
+
+                    ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
+
+                    _totalToBeMintAmount = 0;
+
+                } else {
+
+                    ERC20(_tokenReward).transfer(msg.sender, getReward(msg.sender));
+
+                    _totalToBeMintAmount = _totalToBeMintAmount - getReward(msg.sender);
+
+                }
+
+                
+            }
+
+        }
+        ERC20(_tokenReward).transfer(msg.sender, amount);
+
+        _userInfoMap[msg.sender]._amountDeposite = _userInfoMap[msg.sender]._amountDeposite.sub(amount);
+
+        _userInfoMap[msg.sender]._timeDeposite = block.timestamp;
+    }
+
+
+}
+
+contract NFTStaking is Context, Ownable {
+    
+    using SafeMath for uint256;
+    
+    struct NFTInfo {
+        uint256 commonAmount;
+        uint256 rareAmount;
+        uint256 legendaryAmount;
+        uint256 timeDeposite;
+    }
+
+    IShoes shoes = IShoes(0xf3e07d4d31151C92198374d412893d1d40A1Df99);
+
+    mapping(address => NFTInfo) private userInfoMap;
+
+    address private _tokenReward;
+    uint256 private _totalToBeMintAmount;
+
+    uint256 private _commonShoes = 2;
+    uint256 private _rareShoes = 3;
+    uint256 private _legendaryShoes = 5;
+
+    uint256 private _totalCommon;
+    uint256 private _totalRare;
+    uint256 private _totalLegendary;
+
+    uint256 private _APR = 365 * 24 * 60 * 20 * 100;
+
+    event AddTotalToBeMintAmount(address indexed user, uint256 pendingTotalToBeMintAmount, uint256 totalToBeMintAmount);
+
+    function addTotalToBeMintAmount(uint256 pendingTotalToBeMintAmount) external onlyOwner {
+        require(pendingTotalToBeMintAmount != 0);
+        ERC20(_tokenReward).transferFrom (msg.sender, address(this), pendingTotalToBeMintAmount);
+        _totalToBeMintAmount = _totalToBeMintAmount.add(pendingTotalToBeMintAmount);
+        emit AddTotalToBeMintAmount(msg.sender, pendingTotalToBeMintAmount, _totalToBeMintAmount);
+    }
+
+    function getRewardPerShoes(uint256 nftID, address user) public view returns (uint256 reward) {
+
+        uint256 depositedTime = block.timestamp - userInfoMap[user].timeDeposite;
+
+        uint256 Attribute = shoes.getShoesInformation(nftID);
+
+        if (Attribute == 0) {
+
+            reward = _totalToBeMintAmount.mul(_commonShoes).mul(depositedTime).div(365 days).mul(_APR).div(100).div(_totalCommon);
+
+        } else if (Attribute == 1) {
+
+            reward = _totalToBeMintAmount.mul(_rareShoes).mul(depositedTime).div(365 days).mul(_APR).div(100).div(_totalRare);
+
+        } else {
+
+            reward = _totalToBeMintAmount.mul(_legendaryShoes).mul(depositedTime).div(365 days).mul(_APR).div(100).div(_totalLegendary);
+
+        }
+
+    }
+
+    function getReward(address user) public view returns (uint256 reward) {
+
+        uint256 commonAmount = userInfoMap[user].commonAmount;
+        uint256 rareAmount = userInfoMap[user].rareAmount;
+        uint256 legendaryAmount = userInfoMap[user].legendaryAmount;
+
+        reward = getRewardPerShoes(0, user).mul(commonAmount) + getRewardPerShoes(1, user).mul(rareAmount) + getRewardPerShoes(2, user).mul(legendaryAmount);
+
+    }
+
+    function setAPR(uint256 APR) external onlyOwner {
+        _APR = APR;
+    }
+
+    function getAPR() public view returns (uint256) {
+
+        return _APR;
+
+    }
+
+    function stake(uint256 nftID) external {
+
+        require(shoes.ownerOf(nftID) == msg.sender, "User have to owner of this NFT"); 
+
+        if ((userInfoMap[msg.sender].commonAmount != 0) || (userInfoMap[msg.sender].rareAmount != 0) || (userInfoMap[msg.sender].legendaryAmount != 0)) {
+
+            if (getReward(msg.sender) != 0) {
+
+                if (_totalToBeMintAmount < getReward(msg.sender)){
+
+                    ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
+                    
+                } else {
+
+                    ERC20(_tokenReward).transfer(msg.sender, getReward(msg.sender));
+
+                    _totalToBeMintAmount = _totalToBeMintAmount - getReward(msg.sender);
+
+                }
+            }
+
+        }
+
+        shoes.transferFrom(msg.sender, address(this), nftID);
+
+        uint256 Attribute = shoes.getShoesInformation(nftID);
+
+        if (Attribute == 0) {
+
+            userInfoMap[msg.sender].commonAmount += 1;
+
+        } else if (Attribute == 1) {
+
+            userInfoMap[msg.sender].rareAmount += 1;
+
+        } else {
+
+            userInfoMap[msg.sender].legendaryAmount += 1;
+
+        }
+
+        userInfoMap[msg.sender].timeDeposite = block.timestamp;
+
+
+    }
+
+    function unstake(uint256 nftID) external {
+
+        require(shoes.ownerOf(nftID) == msg.sender, "User have to owner of this NFT"); 
+
+            if (getReward(msg.sender) != 0) {
+
+                if (_totalToBeMintAmount < getReward(msg.sender)){
+
+                    ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
+
+                    _totalToBeMintAmount = 0;
+
+                } else {
+
+                    ERC20(_tokenReward).transfer(msg.sender, getReward(msg.sender));
+
+                    _totalToBeMintAmount = _totalToBeMintAmount - getReward(msg.sender);
+
+                }
+
+                
+            }
+
+        
+        shoes.transferFrom(address(this), msg.sender, nftID);
+
+        uint256 Attribute = shoes.getShoesInformation(nftID);
+
+        if (Attribute == 0) {
+
+            userInfoMap[msg.sender].commonAmount -= 1;
+
+        } else if (Attribute == 1) {
+
+            userInfoMap[msg.sender].rareAmount -= 1;
+
+        } else {
+
+            userInfoMap[msg.sender].legendaryAmount -= 1;
+
+        }
+
+        userInfoMap[msg.sender].timeDeposite = block.timestamp;
+
+    }
+
+
 
 }
 
@@ -56,7 +375,15 @@ contract Operator is Context, Ownable {
 
     using SafeMath for uint256;
 
+    StakingDSoccer sDSoccer;
+
     address private _tokenReward;
+
+    StakingDSoccer private _stakingDSoccer = new StakingDSoccer();
+
+    NFTStaking private _stakingNFT = new NFTStaking();
+
+    address private _stakingShoes;
 
     mapping (address => uint256) private rewardTraining;
 
@@ -126,6 +453,18 @@ contract Operator is Context, Ownable {
     function Footballer() external view returns (address) {
         return address(_footballer);
     }
+
+
+    function Staking() external view returns (address) {
+        return address(_stakingDSoccer);
+    }
+
+    function StakingNFT() external view returns (address) {
+
+        return address(_stakingNFT);
+
+    }
+
 
 
     function Monster() external view returns (address) {
@@ -325,3 +664,4 @@ contract Operator is Context, Ownable {
     receive() external payable {}
 
 }
+
