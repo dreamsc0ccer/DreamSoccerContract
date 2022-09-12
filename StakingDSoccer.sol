d0763ebaedc8d2a917d4e17f829ecad0c6ae601c
dreamsc0ccer <112548430+dreamsc0ccer@users.noreply.github.com>
2022-09-12 16:07:14 +0700

    Update StakingDSoccer.sol

--- StakingDSoccer.sol
+++ StakingDSoccer.sol
@@ -8,7 +8,6 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-
 contract StakingDSoccer is Context, Ownable {
     using SafeMath for uint256;
     
@@ -21,19 +20,36 @@ contract StakingDSoccer is Context, Ownable {
 
     address private _tokenReward;
     uint256 private _totalToBeMintAmount;
-    uint256 private _APY = 365 * 24 * 60 * 20 * 100;
+    uint256 private _APR = 100;
 
     event AddTotalToBeMintAmount(address indexed user, uint256 pendingTotalToBeMintAmount, uint256 totalToBeMintAmount);
 
-    function addTotalToBeMintAmount(uint256 pendingTotalToBeMintAmount) external onlyOwner {
+    address private _TreasuryAddress = 0xd10Aa221f817d98F3f8A33dB67D363e9FE3627BC;
+
+    modifier onlyDev() {	
+        require(_TreasuryAddress == _msgSender(), "Caller is not the dev");	
+        _;	
+    }
+    
+
+    function addTotalToBeMintAmount(uint256 pendingTotalToBeMintAmount) external onlyDev {
         require(pendingTotalToBeMintAmount != 0);
         ERC20(_tokenReward).transferFrom (msg.sender, address(this), pendingTotalToBeMintAmount);
         _totalToBeMintAmount = _totalToBeMintAmount.add(pendingTotalToBeMintAmount);
         emit AddTotalToBeMintAmount(msg.sender, pendingTotalToBeMintAmount, _totalToBeMintAmount);
     }
 
+    function setAPR(uint256 APR) external onlyDev {
 
-    function setTokenReward(address tokenReward) external onlyOwner {
+        require(APR > 0, "APR must be greater than 0");
+
+        _APR = APR;
+    }
+
+    function setTokenReward(address tokenReward) external onlyDev {
+
+        require(tokenReward != address(0), "Token Reward address is not NULL address");
+
         _tokenReward = tokenReward;
     }
 
@@ -41,7 +57,7 @@ contract StakingDSoccer is Context, Ownable {
 
         uint256 depositedTime = block.timestamp - _userInfoMap[msg.sender]._timeDeposite;
 
-        return _userInfoMap[user]._amountDeposite.mul(depositedTime).div(365 days).mul(_APY).div(100);
+        return _userInfoMap[user]._amountDeposite.mul(depositedTime).div(365 days).mul(_APR).div(100);
 
     }
 
@@ -51,11 +67,17 @@ contract StakingDSoccer is Context, Ownable {
 
     }
 
+    function getAPR() public view returns (uint256) {
+
+        return _APR;
+
+    }
+
     function getTotalReward(uint256 startBlock, uint256 endBlock, uint256 amountStake) public view returns (uint256 totalReward) {
 
         uint256 timeStake = endBlock - startBlock;
 
-        return amountStake.mul(timeStake).div(365 days).mul(_APY).div(100);
+        return amountStake.mul(timeStake).div(365 days).mul(_APR).div(100);
 
     }
 
@@ -65,19 +87,22 @@ contract StakingDSoccer is Context, Ownable {
     }
 
     function stake(uint256 amount) external {
+
         require(amount > 0, "Can not stake 0 token");
 
         require(ERC20(_tokenReward).balanceOf(msg.sender) >= amount, "You do not have enough tokens to stake");
 
 
-        if (_userInfoMap[msg.sender]._amountDeposite != 0) {
+        if (_totalToBeMintAmount > 0) {
 
-            if (getReward(msg.sender) != 0) {
+            if (getReward(msg.sender) > 0) {
 
                 if (_totalToBeMintAmount < getReward(msg.sender)){
 
                     ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
-                    
+
+                    _totalToBeMintAmount = 0;
+
                 } else {
 
                     ERC20(_tokenReward).transfer(msg.sender, getReward(msg.sender));
@@ -85,6 +110,8 @@ contract StakingDSoccer is Context, Ownable {
                     _totalToBeMintAmount = _totalToBeMintAmount - getReward(msg.sender);
 
                 }
+
+                
             }
 
         }
@@ -104,9 +131,10 @@ contract StakingDSoccer is Context, Ownable {
 
         require(_userInfoMap[msg.sender]._amountDeposite >= amount, "You do not have enough tokens to unstake");
 
-        if (_userInfoMap[msg.sender]._amountDeposite != 0) {
+        if (_totalToBeMintAmount > 0) {
 
-            if (getReward(msg.sender) != 0) {
+            if (getReward(msg.sender) > 0) {
+
                 if (_totalToBeMintAmount < getReward(msg.sender)){
 
                     ERC20(_tokenReward).transfer(msg.sender, _totalToBeMintAmount);
